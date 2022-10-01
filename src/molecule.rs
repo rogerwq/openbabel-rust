18067e177e734c8601b223c574d2430908fdf858
William Bro-Jørgensen <zrf802@alumni.ku.dk>
2022-10-01 10:12:48 -0700

    move optimization methods to forcefields.rs

--- src/molecule.rs
+++ src/molecule.rs
@@ -9,13 +9,10 @@
 //! assert_eq!(mol.num_atoms(), 6);
 //! ```
 
-use crate::forcefields::{ForceField, Method};
 use ob_rs::ob;
 
 pub struct Molecule {
     pub ob_mol: cxx::UniquePtr<ob::OBMol>,
-    ff_name: ForceField,
-    ff: cxx::UniquePtr<ob::OBForceField>,
 }
 
 impl Molecule {
@@ -23,8 +20,6 @@ impl Molecule {
         cxx::let_cxx_string!(smiles_cxx = smiles);
         Self {
             ob_mol: ob::OBMol_from_smiles(&smiles_cxx),
-            ff_name: ForceField::None,
-            ff: cxx::UniquePtr::null(),
         }
     }
 
@@ -42,82 +37,6 @@ impl Molecule {
     }
     pub fn get_mol_wt(&self) -> f64 {
         ob::OBMol_get_mol_wt(&self.ob_mol)
-    }
-
-    // Optimization routines
-    pub fn optimize(
-        &mut self,
-        force_field: ForceField,
-        method: Method,
-        max_steps: u32,
-        nconv: f64,
-    ) {
-        if self.ff_name == ForceField::None {
-            cxx::let_cxx_string!(ff_name_cxx = force_field.as_str());
-            self.ff = ob::OBForceField_find_forcefield(&ff_name_cxx);
-            self.ff_name = force_field;
-        }
-
-        if ob::OBForceField_is_setup_needed(&self.ff, &self.ob_mol) {
-            ob::OBForceField_setup(&self.ob_mol, &self.ff);
-        }
-        match method {
-            Method::ConjugateGradient => {
-                ob::OBForceField_conjugate_gradients(&self.ff, max_steps, nconv);
-            }
-            Method::SteepestDescent => {
-                ob::OBForceField_steepest_descent(&self.ff, max_steps, nconv);
-            }
-        }
-    }
-
-    /// Initializes the force field. Must be prior to calling [`optimize_n_step`]
-    pub fn initialize_force_field(
-        &mut self,
-        force_field: ForceField,
-        method: Method,
-        max_steps: u32,
-        nconv: f64,
-    ) {
-        if self.ff_name == ForceField::None {
-            cxx::let_cxx_string!(ff_name_cxx = force_field.as_str());
-            self.ff = ob::OBForceField_find_forcefield(&ff_name_cxx);
-            self.ff_name = force_field;
-        }
-        if ob::OBForceField_is_setup_needed(&self.ff, &self.ob_mol) {
-            ob::OBForceField_setup(&self.ob_mol, &self.ff);
-        }
-        match method {
-            Method::ConjugateGradient => {
-                ob::OBForceField_conjugate_gradients_initialize(&self.ff, max_steps, nconv);
-            }
-            Method::SteepestDescent => {
-                ob::OBForceField_steepest_descent_initialize(&self.ff, max_steps, nconv);
-            }
-        }
-    }
-
-    /// Optimizes the molecule `n` steps. Is recommended by OpenBabel if you wish to do stuff in
-    /// between optimization steps as you avoid calling initialization every iteration.
-    pub fn optimize_n_steps(&mut self, method: Method, n: u32) -> Result<bool, ()> {
-        if self.ff_name == ForceField::None {
-            return Err(());
-        }
-        match method {
-            Method::ConjugateGradient => Ok(ob::OBForceField_conjugate_gradients_take_n_steps(
-                &self.ff, n,
-            )),
-            Method::SteepestDescent => {
-                Ok(ob::OBForceField_steepest_descent_take_n_steps(&self.ff, n))
-            }
-        }
-    }
-
-    pub fn energy(&self) -> Option<f64> {
-        match self.ff_name {
-            ForceField::None => None,
-            _ => Some(ob::OBForceField_energy(&self.ff)),
-        }
     }
 }
 
@@ -137,31 +56,5 @@ mod test_mod_molecule {
             "mol wt is {}",
             mol.get_mol_wt()
         );
-    }
-
-    #[test]
-    fn test_direct_optimization() {
-        for ff in [
-            ForceField::GAFF,
-            ForceField::Ghemical,
-            ForceField::MMFF94,
-            ForceField::MMFF94s,
-            ForceField::UFF,
-        ] {
-            let mut mol = Molecule::new_from_smiles("cc");
-            mol.optimize(ff, Method::ConjugateGradient, 4, 1e-5);
-            assert!(mol.energy().unwrap() < 1e-5);
-        }
-    }
-
-    #[test]
-    fn test_stepwise_optimization() {
-        let mut mol = Molecule::new_from_smiles("S1SSSSSSS1");
-        mol.initialize_force_field(ForceField::GAFF, Method::ConjugateGradient, 8, 1e-5);
-        let mut count = 0;
-        while mol.optimize_n_steps(Method::ConjugateGradient, 1).unwrap() {
-            count += 1;
-        }
-        assert!(count > 0);
     }
 }
